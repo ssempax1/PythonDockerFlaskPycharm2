d5b659bebe6491ada15cb17d8acbbfc57d6c71b7
ian <isk5@njit.edu>
2020-07-23 06:45:16 -0400

    Feature ; modified page for style

--- app/app.py
+++ app/app.py
@@ -29,7 +29,7 @@ def cities_import() -> List[Dict]:
 
 @app.route('/')
 def index():
-    user = {'username': 'Miguel'}
+    user = {'username': 'Ianl'}
     cities_data = cities_import()
 
     return render_template('index.html', title='Home', user=user, cities=cities_data)
